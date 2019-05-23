e5f947b9114810fe840b7d669625b8df63c44cb5
Justin Teng <nekasrofd@justins-mbp.lan>
2019-05-23 00:03:49 -0700

    successfully scraped data from website fitting my data schema and added to mongodb database

--- dogFoodScraper.js
+++ dogFoodScraper.js
@@ -1,6 +1,17 @@
 const dogFoodParse = require("./dogFoodParse");
 const createList = require("./listOfFood");
+
+// Get search terms to add onto url
 const searchTerms = createList();
+
+// Connection URL
+const url = "mongodb://localhost:27017/";
+
+// Create a client to connect to mongo service
+const MongoClient = require("mongodb").MongoClient;
+
+// Database Name
+const dbName = "dog-food";
 
 const dogFoodScrape = () => {
   Promise.all(
@@ -10,7 +21,21 @@ const dogFoodScrape = () => {
       );
     })
   ).then(foods => {
-    console.log(foods);
+    // Make client connect to mongo service
+    MongoClient.connect(url, function(err, client) {
+      if (err) throw err;
+      console.log("Connected successfully to the server");
+      const db = client.db(dbName);
+
+      // Insert multiple documents to 'foods' collection using insertMany
+      db.collection("foods").insertMany(foods, function(err, res) {
+        if (err) throw err;
+        console.log(res.insertedCount + " documents inserted");
+
+        // Close connection after insert
+        client.close();
+      });
+    });
   });
 };
 
